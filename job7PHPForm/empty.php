9b7ec220e0ffb6fcebb46006a977b387fbc40cac
Azaria Cindy Sahasika <cindysss099@gmail.com>
2024-10-08 09:17:27 +0700

    job07-prak02

---
+++ job7PHPForm/empty.php
@@ -0,0 +1,14 @@
+<?php
+$myArray = array(); // array empty
+if (empty($myArray)) {
+    echo "Array is empty";
+} else {
+    echo "Array is not empty";
+}
+
+if (empty($nonExistentVar)) {
+    echo "Variable is empty";
+} else {
+    echo "Variable is not empty";
+}
+?>
